b4f94ece80e1152bd87588609867c4b6b690c92a
yechiel chabany <Yechel@users.noreply.github.com>
2018-11-07 15:53:34 +0200

    add model package

--- app/src/main/java/com/nchechik/mini_pro_android_5779/MainActivity.java
+++ app/src/main/java/com/nchechik/mini_pro_android_5779/MainActivity.java
@@ -2,6 +2,7 @@ package com.nchechik.mini_pro_android_5779;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.nchechik.mini_pro_android_5779.R;
 
 public class MainActivity extends AppCompatActivity {
 
