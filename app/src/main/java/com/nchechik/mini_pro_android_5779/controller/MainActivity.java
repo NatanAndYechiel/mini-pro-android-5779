61eb26424bce35640488bad58045590be6472783
nchechik <nich26148@gmail.com>
2018-11-07 15:49:39 +0200

    starting new project

---
+++ app/src/main/java/com/nchechik/mini_pro_android_5779/controller/MainActivity.java
@@ -0,0 +1,17 @@
+package com.nchechik.mini_pro_android_5779.controller;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.nchechik.mini_pro_android_5779.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
